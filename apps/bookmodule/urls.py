4a40191a8aea3668be9f9bf6fa92f19a11e20aff
ProgrammerSam12 <sirhusam2000@gmail.com>
2025-04-17 11:20:53 +0300

    Upload complete Lab 4 with proper structure and styling

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
 
     path('', views.index, name= "books.index"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
     path('list_books/', views.list_books, name= "books.list_books"),
     path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
-    path('aboutus/', views.aboutus, name="books.aboutus")
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -20,9 +20,12 @@ def viewbook(request, bookId):
 
 def index(request):
     return render(request, "bookmodule/index.html")
+
 def list_books(request):
     return render(request, 'bookmodule/list_books.html')
+
 def viewbook(request, bookId):
     return render(request, 'bookmodule/one_book.html')
+
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
